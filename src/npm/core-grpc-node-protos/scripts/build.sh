9b2124b8c849b8b2b2b17a6edc5041e66a930984
devan88 <devan88@users.noreply.github.com>
2022-09-13 01:14:38 +0800

    add Dockerfile for node and workflow use docker to build and push

--- src/npm/core-grpc-node-protos/scripts/build.sh
+++ src/npm/core-grpc-node-protos/scripts/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-cd ../../../protos;
-$output_dir = "../src/npm/core-grpc-protos/protos";
+cd /protos;
+output_dir="/build/protos";
 # grpc-js
 # JavaScript code generating
-grpc_tools_node_protoc.cmd `
-    --js_out=import_style=commonjs,binary:$output_dir `
-    --grpc_out=grpc_js:$output_dir `
+grpc_tools_node_protoc \
+    --plugin=protoc-gen-grpc=/build/node_modules/.bin/grpc_tools_node_protoc_plugin \
+    --js_out="import_style=commonjs,binary:$output_dir" \
+    --grpc_out="grpc_js:$output_dir" \
     ./notifications/*.proto
 
-grpc_tools_node_protoc.cmd `
-    --plugin=protoc-gen-ts=$protoc_gen_ts_path `
-    --ts_out=grpc_js:$output_dir `
+grpc_tools_node_protoc \
+    --plugin=protoc-gen-ts=/build/node_modules/.bin/protoc-gen-ts \
+    --ts_out="grpc_js:$output_dir" \
     ./notifications/*.proto
-#grpc_tools_node_protoc.cmd --js_out=import_style=commonjs,binary:./gen --grpc_out=grpc_js:./gen notification.proto
